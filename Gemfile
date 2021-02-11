7c2a692ea83704bf256a3416e64ad2e16a89282b
”tomofumi03” <kuchibuefuite@icloud.com>
2021-02-11 12:15:45 +0900

    Add account activation

--- Gemfile
+++ Gemfile
@@ -10,8 +10,6 @@ gem 'bcrypt',         '3.1.12'
 gem 'bootstrap-sass', '3.3.7'
 
 gem 'jquery-rails'
-
-gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
@@ -46,6 +44,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'sqlite3', '1.3.13'
 end
 
 group :development do
@@ -66,6 +65,10 @@ group :test do
   gem 'guard',                    '2.16.2'
   gem 'guard-minitest',           '2.4.4'
   gem 'rails-controller-testing'
+end
+
+group :production do
+  gem 'pg', '0.20.0'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,12 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated? 
   end
 
   def new
@@ -16,14 +17,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    #binding.pry
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Piano App!"
-      redirect_to @user
+      #binding.pry
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
-      render :new
+      render 'new'
     end
   end
 
